41c27b907d079fe3bade6bacbaf566149e4599a9
c24io1104 <therealm1ce1@gmail.com>
2025-05-24 16:06:14 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from './server';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
